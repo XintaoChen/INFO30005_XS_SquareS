79239b6c186f8798b1348be906aa8bba621031ca
JesseJia <406046696@qq.com>
2022-04-10 12:21:17 +1000

    static page and single patient page demo
    node app.js
    
    usable router:
    localhost:3001/home
    localhost:3001/aboutDiabetes
    localhost:3001/aboutUs
    localhost:3001/patient/:id
    localhost:3001/patient/000001
    localhost:3001/patient/000002

---
+++ models/peopleModel.js
@@ -0,0 +1,13 @@
+module.exports =
+[
+    {
+        id: '10001',
+        first_name: 'Alice',
+        last_name: 'One'
+    },
+    {
+        id: '10002',
+        first_name: 'Bob',
+        last_name: 'Two'
+    }
+]
