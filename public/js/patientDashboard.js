07981c059cfc10f9c5284acfc1fecb8b145f4271
shuetyi-lee <89139961+shuetyi-lee@users.noreply.github.com>
2022-04-21 13:54:01 +1000

    Michelle - added more functions to edit buttons

--- public/js/patientDashboard.js
+++ public/js/patientDashboard.js
@@ -1,1 +1,35 @@
+// to change edit button to save button
 editIcon = (icon) => icon.classList.toggle('icon-save')
+
+// to display comment box on click
+function editValueComment() {
+    var commentBox = document.getElementById('comment');
+    var valueBox = document.getElementById('value');
+    // var editBtn = document.getElementsByClassName('edit-btn');
+    if (commentBox.style.display == 'none') {
+        commentBox.style.display = 'block';
+    } else {
+        commentBox.style.display = 'none';
+    }
+
+    valueBox.removeAttribute("disabled");
+    //valueBox.setAttribute("disabled", "disabled");
+
+    // for (i=0; i<commentBox.length; i++) {
+    //     if (commentBox[i].style.display == 'none') {
+    //         commentBox[i].style.display = 'block';
+    //     } else {
+    //         commentBox[i].style.display == 'none';
+    //     }
+    // }
+}
+
+// to display current date
+
+var today = new Date();
+var day = today.getDay();
+var daylist = ["Sunday","Monday","Tuesday","Wednesday ","Thursday","Friday","Saturday"];
+var date = today.getDate()+'/'+(today.getMonth()+1)+'/'+today.getFullYear();    
+// var time = today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds();
+// var dateTime = date+' '+time;
+document.getElementById("displayDate").innerHTML = date + ', ' + daylist[day];
