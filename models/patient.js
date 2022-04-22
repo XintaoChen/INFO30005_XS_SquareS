01bf7d8b241744c796213e29868a4a0e8dd6ae25
XintaoChen <xintaoc1@student.unimelb.edu.au>
2022-04-22 14:10:30 +1000

    Justin - finish ajax demo

--- models/patient.js
+++ models/patient.js
@@ -4,7 +4,7 @@ const HealthData = require("./healthData");
 
 const patientSchema = new mongoose.Schema({
   emailAddress: { type: String, required: true, unique: true },
-  password: { type: String, required: true, unique: true },
+  password: { type: String, required: true },
   nameFamily: { type: String, required: true },
   nameGiven: { type: String, required: true },
   dateOfBirth: { type: Date, required: true },
@@ -12,7 +12,6 @@ const patientSchema = new mongoose.Schema({
     type: mongoose.Schema.Types.ObjectId,
     ref: clinicianSchema,
     required: true,
-    unique: true,
   },
   phoneNumber: String,
   homeAddress: String,
