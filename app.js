7a51c2d48a0181c6111cde483fdb0944d54008fb
XintaoChen <xintaoc1@student.unimelb.edu.au>
2022-04-22 13:27:44 +1000

    Justin - ajax setting

--- app.js
+++ app.js
@@ -1,7 +1,9 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const app = express();
+var cors = require("cors");
 
+app.use(cors());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.use(express.urlencoded({ extended: true }));
