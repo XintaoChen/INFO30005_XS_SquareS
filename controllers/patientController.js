b36d10765a93cf4870cd9733379c9a4d46239af5
XintaoChen <xintaoc1@student.unimelb.edu.au>
2022-04-24 20:32:40 +1000

    m

--- controllers/patientController.js
+++ controllers/patientController.js
@@ -2,6 +2,10 @@ const patientData = require('../models/patientModel')
 const { param } = require('../routes/peopleRouter')
 
 const getPatientInfo = (req, res) => {
+
+
+
+    
     const tempData = patientData.find((data) => data.id === req.params.id)
     if (tempData) {
         res.render('singlePatient.hbs', { singlePatientData: tempData })
