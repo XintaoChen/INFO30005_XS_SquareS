583e30690cc7bcbb09679be6e4dc8a71e06481d3
XintaoChen <xintaoc1@student.unimelb.edu.au>
2022-04-24 13:47:17 +1000

    Justin - rewrite js code of patientDashbouad

--- public/js/patientDashboard.js
+++ public/js/patientDashboard.js
@@ -1,41 +1,47 @@
-// import recordItemGenerator from "/js/recordItemGenerator.js"
+// // to change edit button to save button
+// editIcon = (icon) => icon.classList.toggle('icon-save')
 
-// to change edit button to save button
-editIcon = (icon) => icon.classList.toggle('icon-save')
+// // to display comment box on click
+// function editValueComment() {
+//     var commentBox = document.getElementById('comment');
+//     var valueBox = document.getElementById('value');
+//     // var editBtn = document.getElementsByClassName('edit-btn');
+//     if (commentBox.style.display == 'none') {
+//         commentBox.style.display = 'block' ;
+//     } else {
+//         commentBox.style.display = 'none';
+//     }
 
-// to display comment box on click
-function editValueComment() {
-    var commentBox = document.getElementById('comment');
-    var valueBox = document.getElementById('value');
-    // var editBtn = document.getElementsByClassName('edit-btn');
-    if (commentBox.style.display == 'none') {
-        commentBox.style.display = 'block' ;
-    } else {
-        commentBox.style.display = 'none';
-    }
-
-    valueBox.removeAttribute("disabled");
-    //valueBox.classList.remove
-    //valueBox.setAttribute("disabled", "disabled");
+//     valueBox.removeAttribute("disabled");
+//     //valueBox.classList.remove
+//     //valueBox.setAttribute("disabled", "disabled");
     
-    // for (i=0; i<commentBox.length; i++) {
-    //     if (commentBox[i].style.display == 'none') {
-    //         commentBox[i].style.display = 'block';
-    //     } else {
-    //         commentBox[i].style.display == 'none';
-    //     }
-    // }
-}
+//     // for (i=0; i<commentBox.length; i++) {
+//     //     if (commentBox[i].style.display == 'none') {
+//     //         commentBox[i].style.display = 'block';
+//     //     } else {
+//     //         commentBox[i].style.display == 'none';
+//     //     }
+//     // }
+// }
 
-// to display current date
+// // to display current date
 
-var today = new Date();
-var day = today.getDay();
-var daylist = ["Sunday","Monday","Tuesday","Wednesday ","Thursday","Friday","Saturday"];
-var date = today.getDate()+'/'+(today.getMonth()+1)+'/'+today.getFullYear();    
-// var time = today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds();
-// var dateTime = date+' '+time;
-document.getElementById("displayDate").innerHTML = date + ', ' + daylist[day];
+// var today = new Date();
+// var day = today.getDay();
+// var daylist = ["Sunday","Monday","Tuesday","Wednesday ","Thursday","Friday","Saturday"];
+// var date = today.getDate()+'/'+(today.getMonth()+1)+'/'+today.getFullYear();    
+// // var time = today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds();
+// // var dateTime = date+' '+time;
+// document.getElementById("displayDate").innerHTML = date + ', ' + daylist[day];
+
+
+import recordItemGenerator from "/js/recordItemGenerator.js"
+import "/js/completionRateChartPlugin.js"
+
+window.onload = function () {
+    CompletionRateChartPlugin("completion-container", 66)
+};
 
 recordItemGenerator().generate("Blood Glucose Level","record-area")
 recordItemGenerator().generate("Weight","record-area")
--- public/js/recordItemGenerator.js
+++ public/js/recordItemGenerator.js
@@ -1,18 +1,35 @@
 export default function generateRecordItem() {
-  let recordItemStructure = document.createElement("div")
-  recordItemStructure.classList.add("record-item")
-  recordItemStructure.innerHTML = "\
-    <div class='item'></div>\
-    <div class='value-comment'>\
-        <input type='number' placeholder='Enter value' class='value' disabled='true'>\
-        <input type='text' placeholder='Enter comment' class='comment'>\
-    </div>\
-    <div class='edit-save'>\
-        <button class='edit-btn'><i class='icon-edit-patient'></i></button>\
-        <button class='save-btn'><i class='icon-save'></i></button>\
-    </div>\
-  "
+  let init = () => {
+    let recordItemStructure = document.createElement("div")
+    recordItemStructure.classList.add("record-item")
+    recordItemStructure.innerHTML = "\
+      <div class='item'></div>\
+      <div class='value-comment'>\
+          <input type='number' placeholder='Enter value' class='value' disabled='true'>\
+          <input type='text' placeholder='Enter comment' class='comment pdb-hidden'>\
+      </div>\
+    "
+    let editBtn = document.createElement("div")
+    editBtn.classList.add('edit-save')
+    editBtn.innerHTML = "\
+      <button class='edit-btn'><i class='icon-edit-patient'></i></button>\
+      <button class='save-btn'><i class='icon-save'></i></button>\
+    "
+    editBtn.onclick = (e) => {
+      let commentInput = e.target.parentElement.parentElement.previousElementSibling.children[1];
+      if(commentInput.classList.contains("pdb-hidden")){
+        commentInput.classList.remove("pdb-hidden")
+      }else{
+        commentInput.classList.add("pdb-hidden")
+      }
+    }
+    recordItemStructure.appendChild(editBtn)
+    return recordItemStructure
+  }
   let generate=(itemName, containerId)=>{
+    let recordItemStructure = init();
+    let id = itemName.replace(/[ ]/g, "-");
+    recordItemStructure.id=id;
     recordItemStructure.children[0].innerHTML = itemName;
     document.getElementById(containerId).appendChild(recordItemStructure);
   }
