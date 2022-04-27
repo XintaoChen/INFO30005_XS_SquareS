887fe73cd865c06fb80af119db69eaf589fd2160
JesseJia <406046696@qq.com>
2022-04-28 02:36:34 +1000

    Jesse: Finished patient dashboard desktop version.

--- models/clinician.js
+++ models/clinician.js
@@ -1,4 +1,5 @@
 const mongoose = require("mongoose");
+const { version } = require("prettier");
 
 const clinicianSchema = new mongoose.Schema({
   emailAddress: { type: String, required: true, unique: true },
@@ -9,7 +10,7 @@ const clinicianSchema = new mongoose.Schema({
   clinicAddress: String,
   profilePhoto: Buffer,
   briefTextBio: String,
-});
+},{versionKey:false});
 
 const Clinician = mongoose.model("Clinician", clinicianSchema, "Clinician");
 module.exports = Clinician;
