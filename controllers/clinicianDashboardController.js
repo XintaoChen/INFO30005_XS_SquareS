b2aa1f37a0e8630e1b7b81f737551213aa2b7b82
SIMAIY <simaiy@student.unimelb.edu.au>
2022-04-28 22:05:54 +1000

    add title & highlight the data that fall outside the safety thresholds

--- controllers/clinicianDashboardController.js
+++ controllers/clinicianDashboardController.js
@@ -1,88 +1,80 @@
 const Clinician = require('../models/clinician')
-const mongoose = require("mongoose");
+const Patient = require('../models/patient')
+const HealthData = require("../models/healthData");
+const Record = require("../models/record");
 
 const getTodayDataClinician = async (req, res, next) => {
-    try{
-        
-        let tempData = {}
-        const cData = await Clinician.findById(req.params.id).lean()
-        //get all required data form database
-        Clinician.aggregate([
-            {
-                $lookup:{
-                    from: "Patient",
-                    localField: "_id",
-                    foreignField: "clinicianId",
-                    as: 'patient'
+    try {
+        const clinicianId = req.params.id;
+        const untrackedHealthDataList = await HealthData.find({}, "dataName unit");
+        let healthDataList = untrackedHealthDataList.map((healthData) => {
+          return {
+            healthTitle: healthData.dataName + " (" + healthData.unit + ")",
+          };
+        });
+        const clinicianInfo = await Clinician.findById(clinicianId);
+        if (!clinicianInfo) {
+          res.render("noRecords.hbs");
+        }
+    
+        const untrackedPatientList = await Patient.find(
+          { clinicianId: clinicianId },
+          "nameGiven nameFamily recordingData"
+        );
+        let patientList = await Promise.all(
+          untrackedPatientList.map(async (patient) => {
+            let dataList = await Promise.all(
+              untrackedHealthDataList.map(async (dataType) => {
+                let healthDataId = dataType._id;
+                let healthDataMatched = patient.recordingData.find((item) => {
+                  return (
+                    item.healthDataId.toString() == healthDataId.toString() &&
+                    item.isRequired != false
+                  );
+                });
+                let { upperBound, lowerBound } = healthDataMatched || {};
+                let { dataName, unit } = dataType;
+                let isRequired = healthDataMatched ? true : false;
+                let value = undefined;
+                if (isRequired) {
+                  let record = await Record.findOne({
+                    patientId: patient._id,
+                    date: {
+                      $gte: new Date("2022,04,28"),
+                      $lt: new Date("2022,04,29"),
+                    },
+                    healthDataId: healthDataId,
+                  });
+                  if (record) {
+                    value = record.value;
+                  }
                 }
-            },{
-                $match: {
-                    _id: mongoose.Types.ObjectId(req.params.id),
-                }
-            }
-            ,{
-                $skip: 0
-            }
-            ,{
-                $unwind: '$patient' //  You have to use $unwind on an array if you want to use a field in the subdocument array to further usage with `$lookup` 
-              },
-              {
-                $lookup: {
-                  from: "Record",
-                    pipeline: [
-                        { $match: { 
-                            date: {
-                                // change this to current date
-                                $gte: new Date("2022,04,24"),
-                                $lt: new Date("2022,04,25")
-                            }
-                         }
-                        }
-                     ],
-                  localField: "patient._id",
-                  foreignField: "patientId",
-                  as: "recordInfo"
-                }
-              },
-        ], (err, docs) => {
-            if (err) {
-                return console.log(err)
-            }
-            // console.log(JSON.stringify(docs))  
-            tempData = docs
-            if (tempData) {
-                //manipulate data to satisfy .hbs page data logic
-                tempData[0].patientList = []
-                if(tempData.length > 1){
-                    for(i=0; i<tempData.length; i++){
-                        tempData[0].patientList.push(tempData[i].patient)
-                    }
-                }else{
-                    tempData[0].patientList.add(tempData[0].patient)
-                }
-                tempData = tempData[0]
-                for(i=0; i<tempData.recordInfo.length; i++){
-                    for(j=0; j<tempData.patientList.length; j++){
-                        if(tempData.recordInfo[i].patientId.toString() == tempData.patientList[j]._id.toString()){
-                            for(k=0; k<tempData.patientList[j].recordingData.length; k++){
-                                if(tempData.recordInfo[i].healthDataId.toString() == tempData.patientList[j].recordingData[k].healthDataId.toString()){
-                                    tempData.patientList[j].recordingData[k].comment = tempData.recordInfo[i].comment;
-                                    tempData.patientList[j].recordingData[k].value = tempData.recordInfo[i].value;
-                                }
-                            }
-                        }
-                    }
-                }
-                console.log(tempData)  
-                res.render('clinicianDashboard.hbs', { clinicianDashboardData: tempData, clinicianData: cData})
-            } else {
-                res.render('noRecords.hbs')
-            }
-        })
-
-        
-    }catch(err){
-        return next(err)
+                return {
+                  upperBound: upperBound,
+                  lowerBound: lowerBound,
+                  dataName: dataName,
+                  unit: unit,
+                  value: value,
+                  isRequired: isRequired,
+                };
+              })
+            );
+            return {
+              nameGiven: patient.nameGiven,
+              nameFamily: patient.nameFamily,
+              recordingData: dataList,
+            };
+          })
+        );
+        let tempData = {
+          nameGiven: clinicianInfo.nameGiven,
+          patientList: patientList,
+          healthDataList: healthDataList,
+        };
+        console.log(tempData.patientList[0]);
+        res.render('clinicianDashboard.hbs', { clinicianDashboardData: tempData})
+    } catch (err) {
+        return next(err);
     }
 }
 
