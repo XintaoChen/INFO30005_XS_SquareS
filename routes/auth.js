36d99faeaa601fb95d7731837981cad6083c9a1f
JesseJia <406046696@qq.com>
2022-05-07 14:39:54 +1000

    Jesse: finished basic login function

--- routes/auth.js
+++ routes/auth.js
@@ -6,22 +6,19 @@ const router = express.Router()
 const isAuthenticated = (req, res, next) => {
   // If user is not authenticated via passport, redirect to login page 
   if (!req.isAuthenticated()) {
-
     return res.redirect('/login')
-  } else {
-    // id = req.user._id.toString()
   }
   // Otherwise, proceed to next middleware function
   return next()
 }
 // Main page which requires login to access
 // Note use of authentication middleware here
-router.get('/', isAuthenticated, (req, res) => {
-  res.render('home', { title: 'Express', user: req.user.toJSON() })
-})
-router.get('/home', isAuthenticated, (req, res) => {
-  res.render('home', { title: 'Express', user: req.user.toJSON() })
-})
+// router.get('/', isAuthenticated, (req, res) => {
+//   res.render('home', { title: 'Express', user: req.user.toJSON() })
+// })
+// router.get('/home', isAuthenticated, (req, res) => {
+//   res.render('home', { title: 'Express', user: req.user.toJSON() })
+// })
 // Login page (with failure message displayed upon login failure)
 router.get('/login', (req, res) => {
   res.render('login', { flash: req.flash('error'), title: 'Login' })
