ade1fddaf2ca5c012ef075d6da0966566b393b8d
JesseJia <406046696@qq.com>
2022-05-18 21:35:03 +1000

    Jesse: merge Michelle's branch for clinician-single patient page and secure all related routers

--- routes/patientRouter.js
+++ routes/patientRouter.js
@@ -7,26 +7,33 @@ const patientRouter = express.Router()
 // connect to controller
 const patientController = require('../controllers/patientController')
 
+const isAuthenticated = (req, res, next) => {
+    // If user is not authenticated via passport, redirect to login page 
+    if (!req.isAuthenticated()) {
+        return res.redirect('/login')
+    }
+    // Otherwise, proceed to next middleware function
+    return next()
+    }
+
 // process routes by calling controller functions
-patientRouter.get('/:id', patientController.getPatientInfo)
+patientRouter.get('/:id', isAuthenticated, patientController.getPatientInfo)
 //patientRouter.get('/:id', (req, res) => patientController.getPatientNote(req,res))
 
-patientRouter.post('/addNote', function (req, res) {
+patientRouter.post('/addNote', isAuthenticated, function (req, res) {
     patientController.addNote(req.body)
     console.log(req.body)
     res.redirect('/patient/' + req.body.patientId.toString())
 })
 
-patientRouter.post('/updateSupportMessage', function (req, res) {
+patientRouter.post('/updateSupportMessage', isAuthenticated, function (req, res) {
     console.log(req.body)
-
-    
     patientController.updateSupportMessage(req.body)
     console.log(req.body)
     res.redirect('/patient/' + req.body.patientId.toString())
 })
 
-patientRouter.post('/editDataSetting', function (req, res) {
+patientRouter.post('/editDataSetting', isAuthenticated,function (req, res) {
     patientController.editDataSetting(req.body)
     res.redirect('/patient/' + req.body.patientId.toString())
 })
