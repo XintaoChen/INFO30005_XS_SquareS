37e4dcf1508b676bf8f69e7d784c4092692e2839
JesseJia <406046696@qq.com>
2022-05-19 13:28:44 +1000

    Jesse: revise routers

--- app.js
+++ app.js
@@ -82,7 +82,7 @@ app.use((req, res, next) => {
 
 app.use("/patient", patientDashboardRouter);
 
-app.use("/record", recordRouter);
+// app.use("/record", recordRouter);
 
 app.use("/clinician", clinicianDashboardRouter);
 
