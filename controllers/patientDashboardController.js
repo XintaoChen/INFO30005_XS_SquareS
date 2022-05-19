e77f2e52125d4ac9039b4fbab961e27449786cf6
XintaoChen <xintaoc1@student.unimelb.edu.au>
2022-05-20 00:53:48 +1000

    Justin - finish pda

--- controllers/patientDashboardController.js
+++ controllers/patientDashboardController.js
@@ -4,7 +4,7 @@ const mongoose = require("mongoose");
 const patientModel = require("../models/patient");
 const Record = require("../models/record");
 const HealthData = require("../models/healthData");
-const moment = require("moment")
+const moment = require("moment");
 
 const getTodayDataPatient = async (req, res, next) => {
   var today = new Date();
@@ -12,7 +12,7 @@ const getTodayDataPatient = async (req, res, next) => {
     // const tempDataNoRecords = await patientModel.findById(req.params.id).lean()
     const engagementRate = await updateEngagementRate(req.params.id);
     const leaderBoard = await updateLeaderBoard();
-    let tempData = {leaderBoard : leaderBoard} 
+    let tempData = { leaderBoard: leaderBoard };
     //get all required data from database
     patientModel.aggregate(
       [
@@ -51,7 +51,7 @@ const getTodayDataPatient = async (req, res, next) => {
         }
 
         let tempData = docs[0];
-        tempData.leaderBoard = leaderBoard
+        tempData.leaderBoard = leaderBoard;
 
         if (tempData) {
           //manipulate data to satisfy .hbs page data logic
@@ -64,10 +64,11 @@ const getTodayDataPatient = async (req, res, next) => {
                 var tempTime = "";
                 tempData.recordingData[j].comment =
                   tempData.recordInfo[i].comment;
-                tempTime += 
+                tempTime +=
                   tempData.recordInfo[i].date.getHours() < 10
-                  ? "0" + tempData.recordInfo[i].date.getHours()
-                  : tempData.recordInfo[i].date.getHours();                tempTime += ":";
+                    ? "0" + tempData.recordInfo[i].date.getHours()
+                    : tempData.recordInfo[i].date.getHours();
+                tempTime += ":";
                 tempTime +=
                   tempData.recordInfo[i].date.getMinutes() < 10
                     ? "0" + tempData.recordInfo[i].date.getMinutes()
@@ -82,7 +83,7 @@ const getTodayDataPatient = async (req, res, next) => {
             todayHealthData: tempData,
             loggedin: req.isAuthenticated(),
             isPatient: true,
-            pageName: "My Dashboard"
+            pageName: "My Dashboard",
           });
         } else {
           res.redirect("/login");
@@ -113,79 +114,90 @@ const postTodayDataPatient = (req, res) => {
 };
 
 async function updateEngagementRate(id) {
-    const yesterday = moment().subtract(1, 'days').endOf('day');
-    const patientId = mongoose.Types.ObjectId(id);
-    const timeStamp = patientId.getTimestamp();
+  const yesterday = moment().subtract(1, "days").endOf("day");
+  const patientId = mongoose.Types.ObjectId(id);
+  const timeStamp = patientId.getTimestamp();
 
-    const totalDays = yesterday.diff(moment(timeStamp), 'days') + 1;
-    var engRateBool = false;
+  const totalDays = yesterday.diff(moment(timeStamp), "days") + 1;
+  var engRateBool = false;
 
-    const records = await Record.aggregate([
-        {$match: {patientId : patientId, date : {$lte: new Date(yesterday)} }},
-        {$group: { _id: {$dateToString: { format: "%Y-%m-%d", date: "$date" }}, count: {$sum: 1}}}
-    ])
+  const records = await Record.aggregate([
+    { $match: { patientId: patientId, date: { $lte: new Date(yesterday) } } },
+    {
+      $group: {
+        _id: { $dateToString: { format: "%Y-%m-%d", date: "$date" } },
+        count: { $sum: 1 },
+      },
+    },
+  ]);
 
-    const recordDays = records.length;
-    const engagementRate = (recordDays/totalDays * 100).toFixed(1);
+  const recordDays = records.length;
+  const engagementRate = ((recordDays / totalDays) * 100).toFixed(1);
 
-    if (engagementRate >= 80) {
-        engRateBool = true;
-    }
+  if (engagementRate >= 80) {
+    engRateBool = true;
+  }
 
-    const update = {
-        engagementRate : engRateBool
-    }
+  const update = {
+    engagementRate: engRateBool,
+  };
 
-    const result = await patientModel.findByIdAndUpdate(patientId, update, { new : true }).lean();
-    
+  const result = await patientModel
+    .findByIdAndUpdate(patientId, update, { new: true })
+    .lean();
 }
-
-
 
 async function updateLeaderBoard() {
   //Get last Sunday's and this Monday's dates
-  const lastSunday = moment().subtract(1, 'weeks').endOf('isoWeek');
-  const thisMonday = moment().startOf('isoWeek');
+  const lastSunday = moment().subtract(1, "weeks").endOf("isoWeek");
+  const thisMonday = moment().startOf("isoWeek");
 
   //Get all patientIds in mongoDB
-  const patientIds = await patientModel.find().distinct('_id')
+  const patientIds = await patientModel.find().distinct("_id");
 
   //An array to store all engagementRates of patients
-  let engRate = []
+  let engRate = [];
 
   for (const id of patientIds) {
-      //Get profileName of this patient
-      const profileName = await patientModel.findById(id).distinct('profileName')
-      //Retrive date that patient's account was created (first date)
-      const timeStamp = id.getTimestamp();
-      //Calculate number of days between registered day and thisMonday
-      const numDays = thisMonday.diff(moment(timeStamp), 'days') + 1;
+    //Get profileName of this patient
+    const profileName = await patientModel.findById(id).distinct("profileName");
+    //Retrive date that patient's account was created (first date)
+    const timeStamp = id.getTimestamp();
+    //Calculate number of days between registered day and thisMonday
+    const numDays = thisMonday.diff(moment(timeStamp), "days") + 1;
 
-      //Calculate the number of days this patient recorded data
-      //Get all of this patient's records that were recorded before thisMonday
-      //Group the retrieved records by date
-      const records = await Record.aggregate([
-          {$match: {patientId : id, date : {$lte: new Date(lastSunday)} }},
-          {$group: { _id: {$dateToString: { format: "%Y-%m-%d", date: "$date" }}, count: {$sum: 1}}}
-      ])
+    //Calculate the number of days this patient recorded data
+    //Get all of this patient's records that were recorded before thisMonday
+    //Group the retrieved records by date
+    const records = await Record.aggregate([
+      { $match: { patientId: id, date: { $lte: new Date(lastSunday) } } },
+      {
+        $group: {
+          _id: { $dateToString: { format: "%Y-%m-%d", date: "$date" } },
+          count: { $sum: 1 },
+        },
+      },
+    ]);
 
-      //Calculate the number of days this patient recorded data
-      const recordDays = records.length;
+    //Calculate the number of days this patient recorded data
+    const recordDays = records.length;
 
-      //Get engagementRate by numRecordDays/numRegisteredDays
-      const engagementRate = (recordDays/numDays * 100).toFixed(1);
-      const engRateSinglePatient = {patientId : id, profileName : profileName.toString(), engagementRate : engagementRate}
-      engRate.push(engRateSinglePatient);
-
+    //Get engagementRate by numRecordDays/numRegisteredDays
+    const engagementRate = ((recordDays / numDays) * 100).toFixed(1);
+    const engRateSinglePatient = {
+      patientId: id,
+      profileName: profileName.toString(),
+      engagementRate: engagementRate,
+    };
+    engRate.push(engRateSinglePatient);
   }
 
   //Sort array by engagementRate in dsecnding order
-  engRate.sort((x,y) => y.engagementRate - x.engagementRate);
+  engRate.sort((x, y) => y.engagementRate - x.engagementRate);
   //Get top 5 engagementRate
-  leaderBoardEngRate = engRate.slice(0, 5)
+  leaderBoardEngRate = engRate.slice(0, 5);
 
   return leaderBoardEngRate;
-
 }
 
 const getDataAnalysis = async (req, res) => {
@@ -197,19 +209,9 @@ const getDataAnalysis = async (req, res) => {
       new Date().getMonth(),
       new Date().getDate()
     );
-    const time = date.getTime();
-    const day = date.getDay();
-    const numOfDate = date.getDate();
-    const oneDayTime = 24 * 60 * 60 * 1000;
-    let startOfThisWeek = time - day * oneDayTime;
-    let startOfThisMonth = time - numOfDate * oneDayTime;
     const untrackedRecordList = await Record.find(
       {
         patientId: patientId,
-        date: {
-          $gte: startOfThisMonth,
-          $lt: new Date(),
-        },
       },
       "healthDataId value comment date"
     );
@@ -292,7 +294,7 @@ const getDataAnalysis = async (req, res) => {
       healthDataList: healthDataList,
       loggedin: req.isAuthenticated(),
       isPatient: true,
-      pageName: "My Health Data Record"
+      pageName: "My Health Data Record",
     });
   } catch (err) {
     console.log(err);
