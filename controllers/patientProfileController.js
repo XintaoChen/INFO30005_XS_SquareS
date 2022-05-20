2db46902b23b01480ac6b2cfdb0e78dad0cd1cd1
XintaoChen <xintaoc1@student.unimelb.edu.au>
2022-05-20 21:07:18 +1000

    Justin - edit profile patient

--- controllers/patientProfileController.js
+++ controllers/patientProfileController.js
@@ -1,45 +1,43 @@
-const Patient = require('../models/patient')
-const moment = require('moment');
+const Patient = require("../models/patient");
+const moment = require("moment");
 
 const getPatientProfile = async (req, res, next) => {
-try {
+  try {
     const patientId = req.user._id;
     const patient = await Patient.findById(patientId).lean();
     if (!patient) {
-        res.redirect("/login")
+      res.redirect("/login");
     }
-    var formattedDate = moment(patient.dateOfBirth).format('DD/MM/YYYY');
+    var formattedDate = moment(patient.dateOfBirth).format("DD/MM/YYYY");
     patient.dateOfBirth = formattedDate;
 
-    res.render('patientProfile.hbs', {
-        patientData : patient,
-        loggedin: req.isAuthenticated(),
-        isPatient: true,
-        pageName: "My Profile"
+    res.render("patientProfile.hbs", {
+      patientData: patient,
+      loggedin: req.isAuthenticated(),
+      isPatient: true,
+      pageName: "My Profile",
     });
-
-} catch (err) {
+  } catch (err) {
     console.log(err);
-}
+  }
 };
 
 const updatePatientProfile = async (req, res, next) => {
-    try {
-        const filter = { _id: req.user._id };
-        Patient.findOne(filter).then(async (doc) => {
-            doc.profileName = req.body.profileName
-            doc.phoneNumber = req.body.phoneNumber;
-            doc.homeAddress = req.body.homeAddress;
-            doc.briefTextBio = req.body.briefTextBio;
-            doc.password = req.body.password;
-            doc.save();
-            res.redirect("/login");
-            })
-        }
-    catch (error) {
-        console.log("err");
-        res.redirect("/login");
-    }
-}
+  try {
+    const filter = { _id: req.user._id };
+    Patient.findOne(filter).then(async (doc) => {
+      doc.profileName = req.body.profileName;
+      doc.phoneNumber = req.body.phoneNumber;
+      doc.homeAddress = req.body.homeAddress;
+      doc.briefTextBio = req.body.briefTextBio;
+      doc.password = req.body.password ? req.body.password : doc.password;
+      doc.save();
+      res.redirect("/login");
+    });
+  } catch (error) {
+    console.log("err");
+    res.redirect("/login");
+  }
+};
 
 module.exports = { getPatientProfile, updatePatientProfile };
